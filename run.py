4497f3d23d7868e431f7e316e343bf9833e6b87b
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-04-17 18:23:04 +0200

    added dependencies handling
    tries to install dependencies with subprocess.call if there was an ModuleNotFoundError.
    Currently you have to add a "import <module>" statement manually, this is not ideal

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
+try:
+    import termcolor
+except ModuleNotFoundError:
+    print('Trying to install dependencies automatically...')
+    print('If this fails, install them manually by doing')
+    print('pip3 install -r requirements.txt')
+    print('\n'*2)
+
+    import subprocess
+    import os.path
+    requirements_path = os.path.abspath(os.path.join(os.getcwd(), 'requirements.txt'))
+    subprocess.call(['pip3', 'install', '-r', requirements_path])
+
 from Core.main import main
 main()
