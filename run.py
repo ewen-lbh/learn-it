88b3f062f350ee742aad1e286fb0808edf9be9b1
Mx3 <ewen.lebihan7@gmail.com>
2019-04-20 14:03:16 +0200

    add commandline option to edit DATA_FILE
    just do your usual python3 run.py
    but add a "edit" argument, like this:
    
    python3 run.py edit

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
+import sys
+import subprocess
+import webbrowser
+
 try:
     import termcolor
 except ModuleNotFoundError:
     print('Trying to install dependencies automatically...')
     print('If this fails, install them manually by doing')
     print('pip3 install -r requirements.txt')
-    print('\n'*2)
+    print('\n' * 2)
 
     import subprocess
     import os.path
+
     requirements_path = os.path.abspath(os.path.join(os.getcwd(), 'requirements.txt'))
     subprocess.call(['pip3', 'install', '-r', requirements_path])
 
 from src.main import main
+from src.consts import DATA_FILE
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'edit':
+        subprocess.call(['vim', DATA_FILE])
 main()
